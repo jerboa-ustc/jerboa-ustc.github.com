2a456b2fea23928d16fdf89d148771eb603d7662
myn <sa512005@mail.ustc.edu.cn>
2015-03-03 14:57:49 +0800

    light js fancybox函数

--- res/js/light.js
+++ res/js/light.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
         scrolling: 'no',
     });
     // Pictures
-    var images = ["/res/img/bg/01.jpg", "/res/img/bg/02.jpg", "/res/img/bg/03.jpg"];
+    var images = ["/res/img/bg/01.jpg", "/res/img/bg/02.jpg", "/res/img/bg/03.jpg", "/res/img/bg/04.jpg"];
     images.sort(function() {
         return Math.random() > 0.5 ? -1 : 1
     });
